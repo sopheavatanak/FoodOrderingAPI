87d59245826fa498174cf18bef98bfb333e74143
sopheavatanak <sopheavatanak2000@gmail.com>
2026-01-31 00:22:04 +0700

    config file

--- E-Mach_Food-service/Program.cs
+++ E-Mach_Food-service/Program.cs
@@ -1,8 +1,15 @@
+using E_Mach_Food_service;
+using E_Mach_Food_service.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Connect to PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
